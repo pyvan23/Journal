fe78b39a33ebe1e2ae4c257cb92040212332414b
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-07-14 20:30:50 -0300

    end of the layout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,9 @@
 
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView } from "../views/NoteView";
 import { NothingSelectedView } from "../views/NothingSelectedView";
+import {AddOutlined} from '@mui/icons-material'
 
 const drawerWidth = 240
 export const JournalPage = () => {
@@ -12,8 +13,19 @@ export const JournalPage = () => {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim deserunt sunt quasi animi, mollitia repudiandae tempora! Hic quisquam consequuntur ut, perferendis deleniti soluta veniam incidunt enim, amet temporibus adipisci.
     </Typography> */}
 
-    {/* <NothingSelectedView/> */}
-    <NoteView/>
+    <NothingSelectedView/>
+
+    <IconButton size="large"
+    sx={{color:'white',
+    backgroundColor:'error.main',
+    ':hover':{backgroundColor:'error.main',opacity:0.9},
+    position:'fixed',
+    right:50,bottom:50
+    }}>
+      <AddOutlined sx={{fontSize:30}}/>
+      
+    </IconButton>
+    {/* <NoteView/> */}
    </JournalLayout>
   );
 };
