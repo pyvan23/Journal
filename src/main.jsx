74461a60265013a55fbba330d19590f33496c192
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-07-17 18:27:57 -0300

    config redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { JournalApp } from "./JournalApp";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
+  
 );
---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: "checking",
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+  },
+  reducers: {
+    login: (state, action) => {
+
+    },
+    logout: (state, payload) => {
+
+    },
+    checkingCredentials: (state) => {
+        
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { login, logout, checkingCredentials } = authSlice.actions;
