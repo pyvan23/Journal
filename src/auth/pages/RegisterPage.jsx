55c13a1b583bc2cb0395693f170e275259b41c3a
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-07-19 21:48:56 -0300

    validations,useform

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,13 +1,56 @@
-
 import { Link as RouterLink } from "react-router-dom";
-import { Google } from "@mui/icons-material";
+// import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "./layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
+import { useState } from "react";
+
+const data = {
+  email: "iavn@gmail.com",
+  password: "123456",
+  displayName: "ivan",
+};
+
+const formValidations = {
+  email: [(value) => value.includes("@"), "email must be have @"],
+  password: [
+    (value) => value.length >= 6,
+    "password must be have more than 6 characters",
+  ],
+  displayName: [
+    (value) => value.length >= 1,
+    "name must be have more than 1 character",
+  ],
+};
 
 export const RegisterPage = () => {
+  
+  const [formSubmited, setFormSubmited] = useState(false);
+
+  const {
+    displayName,
+    email,
+    password,
+    onInputChange,
+    formState,
+    isFormValid,
+    emailValid,
+    passswordValid,
+    displayNameValid,
+  } = useForm(data, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setFormSubmited(true);
+
+    console.log(formState);
+  };
+
   return (
     <AuthLayout title="Create an account">
-      <form>
+      <h1>Form Valid {isFormValid ? "Valid" : "not ValidForm"}</h1>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -15,6 +58,11 @@ export const RegisterPage = () => {
               type="text"
               placeholder="name"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={!!displayNameValid && formSubmited}
+              helperText={displayNameValid}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -23,26 +71,37 @@ export const RegisterPage = () => {
               type="email"
               placeholder="email@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={emailValid && formSubmited}
+              helperText={emailValid}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
-              label="pass"
+              label="password"
               type="password"
               placeholder="password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={!!passswordValid && formSubmited}
+              helperText={passswordValid}
             />
           </Grid>
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Register
               </Button>
             </Grid>
-           
           </Grid>
           <Grid container direction="row" justifyContent="end">
-            <Typography sx={{mr:1}}>You do not have an account yet?</Typography>
+            <Typography sx={{ mr: 1 }}>
+              You do not have an account yet?
+            </Typography>
             <Link component={RouterLink} color="inherit" to="/auth/login">
               singin
             </Link>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,25 +1,50 @@
-import { useState } from 'react';
+import { Message } from "@mui/icons-material";
+import { useEffect, useMemo, useState } from "react";
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
+  const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+
+  const isFormValid = useMemo(()=>{
+    for(const formValue of Object.keys(formValidation)){
+        if(formValidation[formValue] !== null) return false;
+    }
+    return true
+  },[formValidation]) 
+
   
-    const [ formState, setFormState ] = useState( initialForm );
 
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState,
-            [ name ]: value
-        });
-    }
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-    const onResetForm = () => {
-        setFormState( initialForm );
+  const onResetForm = () => {
+    setFormState(initialForm);
+  };
+  const createValidators = () => {
+    const formCheckedValues = {};
+    for (const formField of Object.keys(formValidations)){
+        const [fn,errorMessage ] = formValidations[formField];
+        formCheckedValues[`${formField} valid`] = fn(formState[formField]) ? null : errorMessage;
     }
+    setFormValidation(formCheckedValues);
+    console.log(formCheckedValues)
+  };
 
-    return {
-        ...formState,
-        formState,
-        onInputChange,
-        onResetForm,
-    }
-}
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+    ...formValidation, 
+    isFormValid
+  };
+};
