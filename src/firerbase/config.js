be8c65779694458f3484d2e2be4d643e62ad7262
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-08-07 11:45:01 -0300

    enviroment variables

--- src/firerbase/config.js
+++ src/firerbase/config.js
@@ -4,15 +4,24 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnviroment } from "../helpers/getEnviroments";
 
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROYECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnviroment();
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWjFWwFczdkvMbtj_V64B2nRxNV-kFzFk",
-  authDomain: "journal-app-9d29f.firebaseapp.com",
-  projectId: "journal-app-9d29f",
-  storageBucket: "journal-app-9d29f.appspot.com",
-  messagingSenderId: "1043338636300",
-  appId: "1:1043338636300:web:4b02fc32d33da3728fd229",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROYECTID,
+  storageBucket:   VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
---
+++ src/helpers/getEnviroments.js
@@ -0,0 +1,10 @@
+
+
+
+export const getEnviroment = () =>{
+    import.meta.env
+
+    return{
+        ...import.meta.env
+    }
+}
